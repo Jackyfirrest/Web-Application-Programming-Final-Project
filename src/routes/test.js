180e298fbdec4713151c046a34b1e82d5cfe0f06
jackyhsu <m0972220819@gmail.com>
2024-11-25 23:55:27 +0800

    test GEMINI Key

--- src/routes/test.js
+++ src/routes/test.js
@@ -1,2 +1,19 @@
-require('dotenv').config();  // 載入 .env 檔案
-const { OpenAI } = require('openai');  // 引入 openai 套件
+require("dotenv").config();
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Explain how AI works";
+
+(async () => {
+    try {
+        const result = await model.generateContent(prompt);
+        console.log(result.response.text());
+    } catch (error) {
+        console.error("Error generating content:", error.message);
+    }
+})();
